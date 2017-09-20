9e212f18fd473979d8a476232fb48aa6dfec4988
Ashley Lobo <Ashley Lobo@LAPTOP-RJJI4SCH>
2017-09-20 13:54:04 +0530

    LatestGui

---
+++ JavaPrison/src/JavaPrisonGUI.java
@@ -0,0 +1,357 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ashley Lobo
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class Prison{
+// HAS A TYPE OF RELATIONSHIP POLICE CONTAINS PRISON
+    /*
+      add prisoner,change guard,change prisoners service,give parole,remove 
+    prioner
+    */
+    static int piD , giD;
+    String type;
+    ArrayList<Prisoner> inmates;
+    ArrayList<Guards> guards;
+    Prison(int code)
+    {
+        piD = 1000 + code;
+        giD = 100+ code;
+        guards = new ArrayList<>();
+        inmates = new ArrayList<>();
+    }
+    void addPrisoner(Prisoner p)
+    {
+        p.status="Active";
+        p.job="None";
+        p.uniqueiD = piD++;
+        System.out.println("Sucefully Added with Unique ID number: " + p.uniqueiD);
+        inmates.add(p);
+    }
+    void addGuard(Guards g)
+    {
+        g.guardCode = giD++;
+        System.out.println("Sucefully Added with GuardCode number: " + g.guardCode);
+        guards.add(g);
+    }
+    void displayPrisoner(){
+        
+        inmates.forEach((p) -> {
+            System.out.println("Name: "+p.name+" Age: "+p.age+" Gender: "+p.gender+" UniqueId: "+p.uniqueiD+" Job Assigned: "+p.job+" Status:"+p.status);
+        });
+    }
+    void displayGuard(){
+        for (Guards p : guards) {
+            System.out.println("Name: "+p.name+" Age: "+p.age+" Gender: "+p.gender+" Post: "+p.post+" Years of Experience:"+p.yearsOfExperience );
+        }//
+    }
+    Prisoner getPrisoner(int ID)
+    {
+           int get = -1;
+        for(Prisoner p : inmates)
+        {
+            if(p.uniqueiD == ID)
+            {
+                get = inmates.indexOf(p);
+                break;
+            }
+        }
+        if(get!=-1)
+             return inmates.get(get);
+        else
+            return null;
+    }
+    void removePrisoner(int ID)
+    {
+        int rem = -1;
+        for(Prisoner p : inmates)
+        {
+            if(p.uniqueiD == ID)
+            {
+                rem = inmates.indexOf(p);
+                break;
+            }
+        }
+        if(rem!=-1)
+            inmates.remove(rem);
+        else
+            System.out.println("Wrong Index");
+    }
+    void parole(Prisoner p,int id){
+        Services s =new Services();
+        if(s.checkAssign(p)){
+            s.removeJob(s.assignment(p),id);
+            p.status="Parole";
+        }
+        else
+            p.status="Parole";
+        }
+}
+
+class Services{
+    int earnings;
+    int numberOfJobs;
+    ArrayList<Prisoner> Cooking;
+    ArrayList<Prisoner> Construction;
+    ArrayList<Prisoner> Carpentry;
+    Services()
+    {
+        Cooking =new ArrayList<>();
+        Construction =new ArrayList<>();
+        Carpentry =new ArrayList<>();
+    }
+    void giveJob(Prisoner p , int ch)
+    {
+        switch(ch)
+        {
+            case 1:
+                p.job = "Cook";
+                Cooking.add(p);
+                break;
+            case 2:
+                p.job = "Constructor";
+                Construction.add(p);
+                break;
+            case 3:
+                p.job = "Carpenter";
+                Carpentry.add(p);
+                break;
+        }
+    }
+    boolean checkAssign(Prisoner p){
+        
+        return p.job.equals("None");
+    }
+    String assignment(Prisoner p){
+        return p.job;
+    }
+    void removeJob(String s,int id){
+        if(s.equals("Cooking")){
+            for (int i=0;i<Cooking.size();i++) {
+                if(Cooking.get(i).uniqueiD==id)
+                    Cooking.remove(i);
+            }
+        }
+        else if(s.equals("Construction")){
+             for (int i=0;i<Construction.size();i++) {
+                if(Construction.get(i).uniqueiD==id)
+                    Construction.remove(i);
+            }
+        }
+        else if(s.equals("Carpenter")){
+             for (int i=0;i<Carpentry.size();i++) {
+                if(Carpentry.get(i).uniqueiD==id)
+                    Carpentry.remove(i);
+            }
+        }
+    }
+    void displayJobs(int ch)
+    {
+        switch(ch)
+        {
+            case 1:
+                Cooking.forEach((p) -> {
+                    System.out.println(p.name + " Id Number: " + p.uniqueiD);
+        });
+                break;
+            case 2:
+                Construction.forEach((p) -> {
+                    System.out.println(p.name + " Id Number: " + p.uniqueiD);
+        });
+                break;
+            case 3:
+                Carpentry.forEach((p) -> {
+                    System.out.println(p.name + " Id Number: " + p.uniqueiD);
+        });
+                break;
+        }
+    }
+}
+class Cells{
+    // cell a for high level etc etc
+    // cells have gaurds and prisoners
+}
+abstract class Person{
+    // will contain for prisoners etc
+    // name, age,gender
+    String name;
+    int age;
+    String gender;
+    Date birthday;
+    Person(String name,int age,String gender)
+    {     
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    birthday = new Date();
+    }
+    Person(String name,int age,String gender , Date bday)
+    {     
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    bday = birthday;
+    }
+}
+class Prisoner extends Person{
+    // level of crime acc level to crime they will be assigned a cell
+    // crime date of relaease
+    // date of joining ,services assigned
+    int uniqueiD;
+    String job;
+    String status;
+    public Prisoner(String name, int age, String gender)
+    {
+        super(name, age, gender);
+    }
+
+    Prisoner(String name, int age, String gender , Date defaul)
+    {
+        super(name, age, gender , defaul);
+    }
+    // level of crime acc level to crime they will be assigned a cell
+    // crime date of relaease
+    // date of joining ,services assigned
+    String getDetails(){
+        return "Name: "+name+" Age: "+age+" Gender: "+gender+" UniqueId: "+uniqueiD+" Job Assigned: "+job;
+    }
+
+}
+class Guards extends Person{
+    // level of posting
+    // experience in years
+    String post;
+    int yearsOfExperience;
+    int guardCode;
+    Scanner sc = new Scanner(System.in).useDelimiter("\\n");
+    public Guards(String name, int age, String gender)
+    {
+        super(name, age, gender);
+        System.out.println("Enter Post & Current Experience");
+        post = sc.next();
+        yearsOfExperience = sc.nextInt();
+    }
+    // level of posting
+    // experience in years
+        
+    
+}
+
+class Date
+{
+    int dd, mm , yy;
+    Date() //Default Constructors
+    {
+        Scanner d = new Scanner(System.in).useDelimiter("\\n");
+        System.out.println("Enter day month year");
+        dd = d.nextInt();
+        mm = d.nextInt();
+        yy = d.nextInt();
+    }
+    Date(int d , int m ,int y) //Parameterized Constructor
+    {
+        d = dd;
+        m = mm;
+        y = yy;
+    }
+    String getDate()
+    {
+        return dd+"-"+mm+"-"+yy;
+    }
+}
+public class JavaPrisonGUI extends javax.swing.JFrame {
+
+    /**
+     * Creates new form JavaPrisonGUI
+     */
+    public JavaPrisonGUI() {
+        initComponents();
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1030, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 714, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(JavaPrisonGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(JavaPrisonGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(JavaPrisonGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JavaPrisonGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JavaPrisonGUI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    // End of variables declaration//GEN-END:variables
+}
