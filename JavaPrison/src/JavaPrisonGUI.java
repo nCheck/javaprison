7f323b2268f2ca63cdbc931c4b97e50ee71b8a68
Ashley Lobo <Ashley Lobo@LAPTOP-RJJI4SCH>
2017-09-20 15:16:27 +0530

    Lot of work remaining

--- JavaPrison/src/JavaPrisonGUI.java
+++ JavaPrison/src/JavaPrisonGUI.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/*/
+
 /**
  *
  * @author Ashley Lobo
@@ -273,8 +268,23 @@ public class JavaPrisonGUI extends javax.swing.JFrame {
     /**
      * Creates new form JavaPrisonGUI
      */
+    Scanner sc = new Scanner(System.in).useDelimiter("\\n");
+       
+        Prison maleP = new Prison(0 );//Male Prisoners
+        Prison femaleP = new Prison(500 ); //Female Prisoners      
+        Prison guard =new Prison(11);//Guard
+        Prisoner[] p = new Prisoner[10]; //Our Jail can hold 10 Prisoners
+        Guards[] g = new Guards[5]; //Only 5 Guards
+        Services s = new Services();
+        int pC = 5 , gC = 0;
+        Prisoner temp;
+        int tempC;
     public JavaPrisonGUI() {
         initComponents();
+        prisonPanel.setVisible(false);
+       guardPanel.setVisible(false);
+       servicePanel.setVisible(false);
+        System.out.println("Welcome to Mumbai Police Prison Department");
     }
 
     /**
@@ -286,33 +296,174 @@ public class JavaPrisonGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
+        controlPanel = new javax.swing.JPanel();
+        prisonSection = new javax.swing.JButton();
+        guardSection = new javax.swing.JButton();
+        serviceSection = new javax.swing.JButton();
+        prisonPanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        guardPanel = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        servicePanel = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jMenu1.setText("File");
-        jMenuBar1.add(jMenu1);
+        prisonSection.setText("PrisonSection");
+        prisonSection.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                prisonSectionActionPerformed(evt);
+            }
+        });
 
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
+        guardSection.setText("GuardSection");
+        guardSection.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                guardSectionActionPerformed(evt);
+            }
+        });
 
-        setJMenuBar(jMenuBar1);
+        serviceSection.setText("Service Section");
+        serviceSection.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                serviceSectionActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("jLabel1");
+
+        javax.swing.GroupLayout prisonPanelLayout = new javax.swing.GroupLayout(prisonPanel);
+        prisonPanel.setLayout(prisonPanelLayout);
+        prisonPanelLayout.setHorizontalGroup(
+            prisonPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(prisonPanelLayout.createSequentialGroup()
+                .addGap(65, 65, 65)
+                .addComponent(jLabel1)
+                .addContainerGap(109, Short.MAX_VALUE))
+        );
+        prisonPanelLayout.setVerticalGroup(
+            prisonPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(prisonPanelLayout.createSequentialGroup()
+                .addGap(131, 131, 131)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 314, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(225, Short.MAX_VALUE))
+        );
+
+        jLabel3.setText("jLabel3");
+
+        javax.swing.GroupLayout guardPanelLayout = new javax.swing.GroupLayout(guardPanel);
+        guardPanel.setLayout(guardPanelLayout);
+        guardPanelLayout.setHorizontalGroup(
+            guardPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(guardPanelLayout.createSequentialGroup()
+                .addGap(110, 110, 110)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(106, Short.MAX_VALUE))
+        );
+        guardPanelLayout.setVerticalGroup(
+            guardPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(guardPanelLayout.createSequentialGroup()
+                .addGap(87, 87, 87)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(72, Short.MAX_VALUE))
+        );
+
+        jLabel2.setText("jLabel2");
+
+        javax.swing.GroupLayout servicePanelLayout = new javax.swing.GroupLayout(servicePanel);
+        servicePanel.setLayout(servicePanelLayout);
+        servicePanelLayout.setHorizontalGroup(
+            servicePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, servicePanelLayout.createSequentialGroup()
+                .addContainerGap(137, Short.MAX_VALUE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(102, 102, 102))
+        );
+        servicePanelLayout.setVerticalGroup(
+            servicePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(servicePanelLayout.createSequentialGroup()
+                .addGap(94, 94, 94)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(187, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout controlPanelLayout = new javax.swing.GroupLayout(controlPanel);
+        controlPanel.setLayout(controlPanelLayout);
+        controlPanelLayout.setHorizontalGroup(
+            controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, controlPanelLayout.createSequentialGroup()
+                .addComponent(prisonSection, javax.swing.GroupLayout.PREFERRED_SIZE, 410, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(guardSection, javax.swing.GroupLayout.PREFERRED_SIZE, 389, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(serviceSection, javax.swing.GroupLayout.PREFERRED_SIZE, 354, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(43, 43, 43))
+            .addGroup(controlPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(prisonPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(guardPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(120, 120, 120)
+                .addComponent(servicePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        controlPanelLayout.setVerticalGroup(
+            controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(controlPanelLayout.createSequentialGroup()
+                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(prisonSection)
+                    .addComponent(guardSection)
+                    .addComponent(serviceSection))
+                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(controlPanelLayout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(prisonPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(controlPanelLayout.createSequentialGroup()
+                        .addGap(228, 228, 228)
+                        .addComponent(servicePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(controlPanelLayout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addComponent(guardPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1030, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(controlPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 1171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(46, 46, 46))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 714, Short.MAX_VALUE)
+            .addComponent(controlPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void prisonSectionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_prisonSectionActionPerformed
+    prisonPanel.setVisible(true);
+       guardPanel.setVisible(false);
+       servicePanel.setVisible(false);   
+       // TODO add your handling code here:
+    }//GEN-LAST:event_prisonSectionActionPerformed
+
+    private void guardSectionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guardSectionActionPerformed
+        prisonPanel.setVisible(false);
+       guardPanel.setVisible(true);
+       servicePanel.setVisible(false);   
+                  // TODO add your handling code here:
+    }//GEN-LAST:event_guardSectionActionPerformed
+
+    private void serviceSectionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_serviceSectionActionPerformed
+        prisonPanel.setVisible(false);
+       guardPanel.setVisible(false);
+       servicePanel.setVisible(true);   
+       
+    }//GEN-LAST:event_serviceSectionActionPerformed
 
     /**
      * @param args the command line arguments
@@ -350,8 +501,15 @@ public class JavaPrisonGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel controlPanel;
+    private javax.swing.JPanel guardPanel;
+    private javax.swing.JButton guardSection;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel prisonPanel;
+    private javax.swing.JButton prisonSection;
+    private javax.swing.JPanel servicePanel;
+    private javax.swing.JButton serviceSection;
     // End of variables declaration//GEN-END:variables
 }
